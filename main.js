92c83f743618d74e325a8cc75b61ee575e681bc6
Jake Miller <hillsighed@gmail.com>
2017-05-06 09:17:40 -0600

    base layout and API POC implemented

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('hi from main')
+console.log('hi from main');
+
+// Deck details
+var deck_id;
+var remaining_cards;
+
+$('#startBtn').click(function() {
+  $.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1', function (result, err){
+    //console.log(results)
+    console.log(result)
+    deck_id = result.deck_id
+    remaining_cards = result.remaining;
+
+  })
+})
+
+$('#drawCard').click(() =>{
+  $.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`, function(result, err) {
+    console.log('result', result)
+  })
+})
