acb8db33c7059d60c578d25fc39a9349e9564f1c
Jake Miller <hillsighed@gmail.com>
2017-05-06 09:36:44 -0600

    creates player hands and POC for drawing a card from a hand is functional

--- main.js
+++ main.js
@@ -5,17 +5,47 @@ var deck_id;
 var remaining_cards;
 
 $('#startBtn').click(function() {
+  // Get a shuffled deck of cards from API
   $.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1', function (result, err){
-    //console.log(results)
     console.log(result)
     deck_id = result.deck_id
-    remaining_cards = result.remaining;
+
+    //Create the player hands
+    // AI Hand
+    $.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=26`, function(result, err) {
+      console.log('result', result)
+
+      result.cards.map((card) => {
+        console.log(card.code)
+
+        // Add to AI Pile
+        $.get(`https://deckofcardsapi.com/api/deck/${deck_id}/pile/ai_hand/add/?cards=${card.code}`, (result, err) => {
+          console.log(result)
+        })
+      })
+    })
+
+    // AI Hand
+    $.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=26`, function(result, err) {
+      console.log('result', result)
+
+      result.cards.map((card) => {
+        console.log(card.code)
+
+        // Add to Player Pile
+        $.get(`https://deckofcardsapi.com/api/deck/${deck_id}/pile/player_hand/add/?cards=${card.code}`, (result, err) => {
+          console.log(result)
+        })
+      })
+    })
+
+
 
   })
 })
 
 $('#drawCard').click(() =>{
-  $.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`, function(result, err) {
+  $.get(`https://deckofcardsapi.com/api/deck/${deck_id}/pile/player_hand/draw/`, function(result, err) {
     console.log('result', result)
   })
 })
